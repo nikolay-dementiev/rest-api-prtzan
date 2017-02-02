99ebd8a8233d539092739a54ffcd58841ed08fa9
Dementiev Nikolay <dementievnikolay@gmail.com>
2017-02-02 09:37:54 +0200

    initial commit

---
+++ REST-api-test-prtzn/REST-api-test-prtzn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  REST-api-test-prtzn
+//
+//  Created by Nikolay Dementiev on 02.02.17.
+//  Copyright © 2017 mc373. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
